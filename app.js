71aac0817a15b19e6c1e14533086443078525c1c
Prashant Trivedy <pat.trivedy@gmail.com>
2019-03-26 12:56:51 +0530

    Authz code implementation WIP

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const db = require('./db')
+const randomstring = require('randomstring')
 
 const app = express()
 
@@ -9,6 +11,66 @@ app.get('/', (req, res) => {
 })
 
 app.get('/authorize', (req, res) =>{
+
+    //Client ID validation
+    const clientID = req.query.client_id
+    if(!clientID){
+        return res.status(400).render('error', {message: 'Missing required parameter: client_id'})
+    }
+
+
+    const client = db.getClientByID(clientID)
+    if(!client){
+        return res.status(400).render('error', {message: 'Invalid client_id'})
+    }
+
+    //Response type validation
+    const responseType = req.query.response_type
+    if(!responseType){
+        return res.status(400).render('error', {message: 'Missing required parameter: response_type'})
+    }
+    
+    const supportedResTypes = ['code']
+
+    if(!supportedResTypes.includes(responseType)){
+        return res.status(400).render('error', {message: 'Invalid response_type'})  
+    }
+
+    //redirect uri validation
+    const validUris = client.redirect_uris
+    if(!validUris || !validUris.length){
+        return res.status(400).render('error', {message: 'No redirect URIs configured for the client'})
+    }
+
+    let redirectUri = req.query.redirect_uri || null
+    if(redirectUri && !validUris.includes(redirectUri)){
+        return res.status(400).render('error', {message: 'Invalid redirect_uri: ' + redirectUri})
+    }
+
+    if(!redirectUri){
+        redirectUri = validUris[0]
+    }
+
+    //Check session
+    // If no session, redirect to /login
+
+
+    //Create authz code
+    const code = randomstring.generate({length: 32, charset: 'alphanumeric'})
+    const expiresAt = moment().add(10, 'minutes').valueOf()
+    const context = {
+        code,
+        expiresAt,
+        clientID,
+        redirectUri, 
+        user_id: '1'
+    }
+
+    //Save code in db
+    db.saveCodeContext(context)
+
+    const url = new URL(redirectUri)
+
     res.status(501).render('error', {message: '/authorize is not implemented yet'})
 })
 
