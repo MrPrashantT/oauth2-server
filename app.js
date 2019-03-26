d8b1fb960898cbd467b44f05c074b8d2e55a6794
Prashant Trivedy <pat.trivedy@gmail.com>
2019-03-26 16:13:52 +0530

    implemented /token endpoint with validation according to Oauth 2.0 spec - Work in progress

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const db = require('./db')
+const moment = require('moment')
 const randomstring = require('randomstring')
 
 const app = express()
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 
 app.set('view engine', 'ejs')
 
@@ -49,6 +52,7 @@ app.get('/authorize', (req, res) =>{
 
     if(!redirectUri){
         redirectUri = validUris[0]
+
     }
 
     //Check session
@@ -62,7 +66,7 @@ app.get('/authorize', (req, res) =>{
         code,
         expiresAt,
         clientID,
-        redirectUri, 
+        redirectUri: req.query.redirect_uri, 
         user_id: '1'
     }
 
@@ -70,8 +74,99 @@ app.get('/authorize', (req, res) =>{
     db.saveCodeContext(context)
 
     const url = new URL(redirectUri)
+    url.searchParams.set('code', code)
 
-    res.status(501).render('error', {message: '/authorize is not implemented yet'})
+    res.redirect(url)
+})
+
+app.post('/token', (req, res) =>{
+    const body = req.body || {}
+    const grant = body.grant_type
+    if(!grant){
+        return res.status(400).json({error: 'invalid_request', error_description: 'Grant type not specified'});
+    }
+
+    const supportedGrantTypes = ['authorization_code']
+
+    if(!supportedGrantTypes.includes(grant)){
+        return res.status(400).json({error: 'unsupported_grant_type', error_description: 'Grant type not supported'});
+    }
+
+    const auth = req.headers['authorization']
+
+    let client = null
+
+    if(auth){
+        //basic auth
+        const parts = auth.trim().split(' ')
+        if(parts.length !== 2 || parts[0].toLowerCase() !== 'basic'){
+            res.set('WWW-Authenticate', 'Basic')
+            return res.status(401).json({error: 'invalid_client', error_description: 'Unsupported authentication method'})
+        }
+
+        const creds = Buffer.from(parts[1], 'base64').toString('ascii').split(':')
+        client = db.getClientByID(creds[0])
+
+        if(!client || client.secret !== creds[1]){
+            res.set('WWW-Authenticate', 'Basic')
+            return res.status(401).json({error: 'invalid_client', error_description: 'Invalid client ID or secret'})
+        }
+    } else {
+        //json body auth
+        if(!body.client_id || !body.client_secret){
+            return res.status(400).json({error: 'invalid_client', error_description: 'Client auth failed'})
+        }
+
+        client = db.getClientByID(body.client_id)
+        if(!client || client.secret !== body.client_secret){
+            return res.status(401).json({error: 'invalid_client', error_description: 'Invalid client or secret'})
+        }
+    }
+
+    if(!body.code){
+        return res.status(400).json({error:'invalid_request', error_description: 'Missing required parameter: code'})
+    }
+
+    const ctx = db.getCodeContext(body.code)
+
+    if(!ctx){
+        return res.status(400).json({error:'invalid_grant', error_description: 'Invalid authorization code'})
+    }
+
+    db.deleteCodeContext(body.code)
+
+    if(moment().isAfter(ctx.expiresAt)){
+        return res.status(400).json({error:'invalid_grant', error_description: 'Expired authorization code'})
+    }
+
+    if(ctx.clientID !== client.id){
+        return res.status(400).json({error:'invalid_grant', error_description: 'Invalid authorization code'})
+    }
+
+    if(ctx.redirectUri){
+        if(body.redirect_uri !== ctx.redirect_uri){
+            return res.status(400).json({error:'invalid_grant', error_description: 'Invalid redirect uri'})
+        }
+    }
+
+    const token = 'at-' + randomstring.generate({length: 32, charset: 'alphanumeric'})
+    const tokenCtx = {
+        token,
+        expiresAt: moment().add(120, 'minutes').valueOf(),
+        clientID: ctx.clientID,
+        user_id: ctx.user_id
+    }
+
+    db.saveAccessToken(tokenCtx)
+
+
+    res.set('Cache-Control', 'no-store')
+    res.set('Pragma', 'no-cache')
+    res.status(200).json({
+        access_token: token,
+        token_type: 'Bearer',
+        expires_in: 120 * 60
+    })
 })
 
 const PORT = 8500
--- db.js
+++ db.js
@@ -2,7 +2,9 @@ const CLIENTS = [
     { id: '1', secret: 'secret1', redirect_uris: ['https://thameera.com/callback']}
 ]
 
-const AUTHZ_CODES = []
+let AUTHZ_CODES = []
+
+const ACCESS_TOKENS = []
 
 const getClientByID = id => {
     return CLIENTS.find(c => c.id === id)
@@ -12,7 +14,22 @@ const saveCodeContext = ctx => {
     AUTHZ_CODES.push(ctx)
 }
 
+const getCodeContext = code => {
+    return AUTHZ_CODES.find(ctx => ctx.code == code)
+}
+
+const deleteCodeContext = code => {
+    AUTHZ_CODES = AUTHZ_CODES.filter(ctx => ctx.code !== code)
+}
+
+const saveAccessToken = ctx => {
+    ACCESS_TOKENS.push(ctx)
+}
+
 module.exports = {
     getClientByID,
-    saveCodeContext
+    saveCodeContext,
+    getCodeContext,
+    deleteCodeContext,
+    saveAccessToken
 }
